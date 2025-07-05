dc899dafe017d7c71b076f1e013f39159a70f3c4
Priyanshu Pradhan <118527847+Bully26@users.noreply.github.com>
2025-07-06 04:30:31 +0530

    Update redis.js

--- api-server/redis.js
+++ api-server/redis.js
@@ -3,14 +3,13 @@ import { dirname, join } from 'path';
 import dotenv from 'dotenv';
 import Redis from 'ioredis';
 
-// ESM-compatible __dirname
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-// Load .env from one level up
+
 dotenv.config({ path: join(__dirname, '../.env') });
 
-// Initialize Redis
 const redis = new Redis({
   host:  'redis',
   port: 6379,
